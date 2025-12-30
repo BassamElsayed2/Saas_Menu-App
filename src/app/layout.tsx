6ee5776c0fe578851ce9016f516f61d22a5099ae
elboghdady <hadeerelboghdady54@gmail.com>
2025-12-30 23:03:22 +0200

    edit landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Trezo - Tailwind Nextjs Admin Dashboard Template",
+  description: "Tailwind Nextjs Admin Dashboard Template",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <html suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  );
 }
