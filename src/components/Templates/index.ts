5fd6d65a8dc88c804b0ea0cf8e14f6833342c5f0
Bassam-Elsaied <93853900+Bassam-Elsaied@users.noreply.github.com>
2026-01-01 14:53:07 +0200

    fix translate & fix template

--- src/components/Templates/index.ts
+++ src/components/Templates/index.ts
@@ -1,8 +1,5 @@
-// Export components (for backward compatibility)
-export { default as DelightfulMenus } from "./DefaultTemplate";
+// Export main component
 export { default as DefaultTemplate } from "./DefaultTemplate";
-export { default as Template2 } from "./Template2";
-export { default as Template3 } from "./Template3";
 
 // Export registry and types
 export * from "./registry";
